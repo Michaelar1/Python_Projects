4b50514bc76b540200f6d4a28e48a90ef4e376fc
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-02-27 14:49:46 -0500

    Create Bool.py

---
+++ Bool.py
@@ -0,0 +1,19 @@
+
+
+x = 50
+y = 42
+
+if x < y:
+    print("50 is smaller than 42.")
+elif x == y:
+    print("50 is equal to 42.")
+else:
+    print("50 is larger than 42.")
+
+
+print(bool(x))
+print(bool(y))
+print(bool(None))
+
+print(isinstance(x, int))
+print(isinstance(y, str))
