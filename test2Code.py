41e84dac92ba1ef1a468914d35c1bad353610b29
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-03-06 14:40:41 -0500

    Created test2Code.py
    A quick demonstration on notating in Python

---
+++ test2Code.py
@@ -0,0 +1,15 @@
+
+
+# This is my note.
+
+""" This is my note
+and this is still my note
+and this again is still my note
+"""
+
+def printMe():
+    """ this is my docstring
+    """
+    print("Meeeee!")
+    
+print(printMe.__doc__)
