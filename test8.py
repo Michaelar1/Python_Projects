e90d4b5b11a3cb54368552a7c2a4ceba9e444055
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-03-16 17:13:30 -0400

    Created test.db, test8.py

---
+++ test8.py
@@ -0,0 +1,16 @@
+
+
+import sqlite3
+
+conn = sqlite3.connect('test.db')
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_persons( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_fname TEXT, \
+        col_lname TEXT, \
+        col_email TEXT \
+        )")
+    conn.commit()
+conn.close()
