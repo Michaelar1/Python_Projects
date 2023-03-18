88e4cbf149fd3677616fb4ab51f9eee6d95fd945
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-03-18 11:46:35 -0400

    Update Polymorphism_Assignment.py
    Fixed bugs

--- Polymorphism_Assignment.py
+++ Polymorphism_Assignment.py
@@ -7,10 +7,10 @@ class Person:
 
     def getInfo(self):
         input_name = input("Please enter a name: ")
-            if (input_name == self.name):
-                print("\nName: {} \nContact: {} \nOccupation: {}".format(self.name, self.email, self.occupation))
-            else:
-                print("\n{} Not Found".format(input_name))
+        if (input_name == self.name):
+            print("\nName: {} \nContact: {} \nOccupation: {}".format(self.name, self.email, self.occupation))
+        else:
+             print("\n{} Not Found".format(input_name))
 
 # Child Class Metahuman
 class Metahuman(Person):
@@ -23,26 +23,26 @@ class Metahuman(Person):
 
     def getInfo(self):
         input_name = input("Please enter a name: ")
-            if (input_name == self.name):
-                print("\nName: {} \nContact: {} \nAffiliation: {}".format(self.name, self.email, self.affiliation))
-            else:
-                print("\n{} Not Found".format(input_name))
+        if (input_name == self.name):
+            print("\nName: {} \nContact: {} \nAffiliation: {}".format(self.name, self.email, self.affiliation))
+        else:
+             print("\n{} Not Found".format(input_name))
         
 
 # Child Class Universe
 class Superhero(Person):
-    comic_universe: "DC"
-    tv_universe: "Arrowverse"
+    comic_universe = "DC"
+    tv_universe = "Arrowverse"
 
     # Again, the same method is used, but since Barry exists in a superhero universe,
     # It can replace occupation or affiliation
 
     def getInfo(self):
         input_name = input("Please enter a name: ")
-            if (input_name == self.name):
-                print("\nName: {} \nContact: {} \nComic Universe: {}".format(self.name, self.email, self.comic_universe))
-            else:
-                print("\n{} Not Found".format(input_name))
+        if (input_name == self.name):
+            print("\nName: {} \nContact: {} \nComic Universe: {}".format(self.name, self.email, self.comic_universe))
+        else:
+            print("\n{} Not Found".format(input_name))
     
     
                 
