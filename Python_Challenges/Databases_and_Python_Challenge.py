4ed40bcf24d19803984a11febb1abde60a98dc55
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-03-20 13:53:39 -0400

    Updated Databases_And_Python_Challenge.py

--- Python_Challenges/Databases_and_Python_Challenge.py
+++ Python_Challenges/Databases_and_Python_Challenge.py
@@ -3,19 +3,28 @@
 import sqlite3
 from sqlite3 import *
 
+#   One of the requirements for the challenge is to make the db in RAM
+conn = sqlite3.connect(':memory:')
+
 rosterValues = (('Jean-Baptiste Zorg', 'Human', 122), ('Korben Dallas', 'Meat Popsicle', 100), ('Ak\'not', 'Mangalore', -5))
 
 def create_connection():
-    conn = sqlite3.connect(':memory:')
-    c = connect.cursor()
-    c.execute("CREATE TABLE Roster(Name TEXT, Species TEXT, IQ INT")
-    c.execute("INSERT INTO Roster VALUES (?, ?, ?)", rosterValues)
+    c = conn.cursor()
+    c.execute("CREATE TABLE Roster(Name TEXT, Species TEXT, IQ INT)")
+    conn.commit()
+    
+    c.executemany("INSERT INTO Roster VALUES (?, ?, ?)", rosterValues)
+    conn.commit()
         
     c.execute("UPDATE Roster SET Species = ? WHERE Name = ?", ('Human', 'Korben Dallas'))
+    conn.commit()
 
-    c.execute("SELECT Name, IQ FROM Roster WHERE Species = Human")
-    while True:
-        row = c.fetchone()
-        if row is None:
-            break
+    c.execute("DELETE FROM Roster WHERE Name = ?", ('Ak\'not',))
+    conn.commit()
+
+    c.execute("SELECT Name, IQ FROM Roster")
+    rows = c.fetchall()
+    for row in rows:
         print(row)
+
+create_connection()
