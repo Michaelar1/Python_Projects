c603ce02e8ecf4d17d8351137da67c306fa7d8ea
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-03-24 17:38:30 -0400

    updated views.py

--- Django_Challenges/Django_Checkbook_Project/BlueBirdBanking/Checkbook/views.py
+++ Django_Challenges/Django_Checkbook_Project/BlueBirdBanking/Checkbook/views.py
@@ -1,29 +1,33 @@
-from django.shortcuts import render
-from . forms import AccountForm, TransactionForm
+from django.shortcuts import render, redirect
+from .forms import AccountForm, TransactionForm
+
 
 #   This function will render the Home page when requested
 def home(request):
     return render(request, 'checkbook/index.html')
 
+
 #   This function will render the Create New Account page when requested
 def create_account(request):
-    form = AccountForm(data=request.POST or None) #  Retrieves the Account Form
+    form = AccountForm(data=request.POST or None)  # Retrieves the Account Form
     #   Checks if request method is POST
     if request.method == 'POST':
-        if form.is_valid(): #   Checks to see if the submitted form is valid; if so, it saves the form
-            form.save()     #   Saves new account
-            return redirect('index')    #   Returns user back to the home page
-    content = {'form': form}    #   Saves content to the template as a dictionary
+        if form.is_valid():  # Checks to see if the submitted form is valid; if so, it saves the form
+            form.save()  # Saves new account
+            return redirect('index')  # Returns user back to the home page
+    content = {'form': form}  # Saves content to the template as a dictionary
     #   Adds content of form to page
     return render(request, 'checkbook/CreateNewAccount.html')
+
 
 #   This function will render the Balance page when requested
 def balance(request):
     return render(request, 'checkbook/BalanceSheet.html')
 
+
 #   This function will render the Transaction page when requested
 def transaction(request):
-    form = TransactionForm(data=request.POST or None)   #   Retrieves the Transaction Form
+    form = TransactionForm(data=request.POST or None)  # Retrieves the Transaction Form
     #   Checks if request method is POST
     if request.method == 'POST':
         if form.is_valid():
@@ -31,5 +35,3 @@ def transaction(request):
             return redirect('index')
     content = {'form': form}
     return render(request, 'checkbook/AddTransaction.html')
-
-
