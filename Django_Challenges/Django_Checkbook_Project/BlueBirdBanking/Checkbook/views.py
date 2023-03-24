6c468a273a2ec87074dbe560e71110ab60808bd2
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-03-24 16:39:20 -0400

    Updated views.py

--- Django_Challenges/Django_Checkbook_Project/BlueBirdBanking/Checkbook/views.py
+++ Django_Challenges/Django_Checkbook_Project/BlueBirdBanking/Checkbook/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+#   This function will render the Home page when requested
+def home(request):
+    return render(request, 'checkbook/index.html')
+
+#   This function will render the Create New Account page when requested
+def create_account(request):
+    return render(request, 'checkbook/CreateNewAccount.html')
+
+#   This function will render the Balance page when requested
+def balance(request):
+    return render(request, 'checkbook/BalanceSheet.html')
+
+#   This function will render the Transaction page when requested
+def transaction(request):
+    return render(request, 'checkbook/AddTransaction.html')
